b6751e050704d581eb2174227caa8b2f28fb6278
kkj53051000 <tkarnrwl7418@naver.com>
2022-05-29 12:52:00 +0900

    up (+Test), add LandmarkReviewControllerTest

--- src/main/java/com/tripfestival/request/landmark/LandmarkReviewProcessRequest.java
+++ src/main/java/com/tripfestival/request/landmark/LandmarkReviewProcessRequest.java
@@ -1,8 +1,14 @@
 package com.tripfestival.request.landmark;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LandmarkReviewProcessRequest {
     private String content;
     private Byte score;
---
+++ src/test/java/com/tripfestival/controller/landmark/LandmarkReviewControllerTest.java
@@ -0,0 +1,159 @@
+package com.tripfestival.controller.landmark;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.tripfestival.controller.BaseControllerTest;
+import com.tripfestival.domain.landmark.Landmark;
+import com.tripfestival.domain.landmark.LandmarkReview;
+import com.tripfestival.domain.user.User;
+import com.tripfestival.domain.world.WorldCountry;
+import com.tripfestival.domain.world.WorldCountryCity;
+import com.tripfestival.domain.world.WorldCountryCityRegion;
+import com.tripfestival.repository.landmark.LandmarkRepository;
+import com.tripfestival.repository.landmark.LandmarkReviewRepository;
+import com.tripfestival.repository.user.UserRepository;
+import com.tripfestival.repository.world.WorldCountryCityRegionRepository;
+import com.tripfestival.repository.world.WorldCountryCityRepository;
+import com.tripfestival.repository.world.WorldCountryRepository;
+import com.tripfestival.request.landmark.LandmarkReviewProcessRequest;
+import com.tripfestival.vo.Response;
+import com.tripfestival.vo.ResponseVo;
+import com.tripfestival.vo.landmark.LandmarkReviewListVo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LandmarkReviewControllerTest extends BaseControllerTest {
+
+    @Autowired
+    WorldCountryRepository worldCountryRepository;
+
+    @Autowired
+    WorldCountryCityRepository worldCountryCityRepository;
+
+    @Autowired
+    WorldCountryCityRegionRepository worldCountryCityRegionRepository;
+
+    @Autowired
+    LandmarkRepository landmarkRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    LandmarkReviewRepository landmarkReviewRepository;
+
+    WorldCountryCity worldCountryCity;
+    WorldCountryCityRegion worldCountryCityRegion;
+    Landmark landmark;
+    LandmarkReview landmarkReview;
+
+
+    @BeforeEach
+    void setup() {
+        WorldCountry worldCountry = WorldCountry.builder()
+                .name("korea")
+                .build();
+        worldCountryRepository.save(worldCountry);
+
+        worldCountryCity = WorldCountryCity.builder()
+                .name("seoul")
+                .worldCountry(worldCountry)
+                .build();
+        worldCountryCityRepository.save(worldCountryCity);
+
+        worldCountryCityRegion = WorldCountryCityRegion.builder()
+                .name("gangnamgu")
+                .worldCountryCity(worldCountryCity)
+                .build();
+        worldCountryCityRegionRepository.save(worldCountryCityRegion);
+
+        landmark = Landmark.builder()
+                .name("landmark")
+                .worldCountryCityRegion(worldCountryCityRegion)
+                .build();
+        landmarkRepository.save(landmark);
+
+        User user = User.builder()
+                .uid("user")
+                .build();
+        userRepository.save(user);
+
+        landmarkReview = LandmarkReview.builder()
+                .content("riview")
+                .user(user)
+                .landmark(landmark)
+                .build();
+        landmarkReviewRepository.save(landmarkReview);
+    }
+
+    @Test
+    void LANDMARK_REVIEW_PROCESS_TEST() throws Exception {
+        //given
+        LandmarkReviewProcessRequest landmarkReviewProcessRequest = LandmarkReviewProcessRequest.builder()
+                .content("test")
+                .landmarkId(landmark.getId())
+                .build();
+
+        String req = objectMapper.writeValueAsString(landmarkReviewProcessRequest);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/api/landmarkReviewProcess")
+                .contentType(jsonMediaType)
+                .content(req));
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(new ResponseVo(Response.SUCCESS, null))))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    void LANDMARK_REVIEW_REMOVE_TEST() throws Exception {
+        //given setup()
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/api/landmarkReviewRemove/" + landmarkReview.getId()));
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(new ResponseVo(Response.SUCCESS, null))))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    void LANDMARK_REVIEW_LIST_TEST() throws Exception {
+        //given
+        List<LandmarkReview> landmarkReviewList = new ArrayList<>();
+        landmarkReviewList.add(landmarkReview);
+
+        LandmarkReviewListVo landmarkReviewListVo = new LandmarkReviewListVo(landmarkReviewList);
+
+        String response = objectMapper.writeValueAsString(landmarkReviewListVo);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get("/api/landmarkReviewList")
+                .param("landmarkId", String.valueOf(landmark.getId())));
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(response))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
