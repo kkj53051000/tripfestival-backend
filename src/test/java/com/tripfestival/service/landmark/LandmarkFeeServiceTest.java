cdcfe2728f0a020da24fd4ccf8ea5726652e6358
kkj53051000 <tkarnrwl7418@naver.com>
2022-06-13 10:31:03 +0900

    fix LandmarkFeeServiceTest : add (All TEST)

--- src/test/java/com/tripfestival/service/landmark/LandmarkFeeServiceTest.java
+++ src/test/java/com/tripfestival/service/landmark/LandmarkFeeServiceTest.java
@@ -1,9 +1,11 @@
 package com.tripfestival.service.landmark;
 
 import com.tripfestival.domain.landmark.Landmark;
+import com.tripfestival.domain.landmark.LandmarkFee;
 import com.tripfestival.domain.world.WorldCountry;
 import com.tripfestival.domain.world.WorldCountryCity;
 import com.tripfestival.domain.world.WorldCountryCityRegion;
+import com.tripfestival.dto.landmark.LandmarkFeeModifyDto;
 import com.tripfestival.repository.landmark.LandmarkFeeRepository;
 import com.tripfestival.repository.landmark.LandmarkRepository;
 import com.tripfestival.repository.world.WorldCountryCityRegionRepository;
@@ -12,14 +14,19 @@ import com.tripfestival.repository.world.WorldCountryRepository;
 import com.tripfestival.request.landmark.LandmarkFeeProcessRequest;
 import com.tripfestival.vo.Response;
 import com.tripfestival.vo.ResponseVo;
-import org.junit.jupiter.api.Assertions;
+import com.tripfestival.vo.landmark.LandmarkFeeAllListVo;
+import com.tripfestival.vo.landmark.LandmarkFeeListVo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -47,6 +54,7 @@ class LandmarkFeeServiceTest {
     private final ResponseVo SUCCESS_RESPONSE_VO = new ResponseVo(Response.SUCCESS, null);
 
     Landmark landmark;
+    LandmarkFee landmarkFee;
 
     @BeforeEach
     void setup() {
@@ -75,6 +83,13 @@ class LandmarkFeeServiceTest {
                 .worldCountryCityRegion(worldCountryCityRegion)
                 .build();
         landmarkRepository.save(landmark);
+
+        landmarkFee = LandmarkFee.builder()
+                .title("landmarkFee")
+                .price(100)
+                .landmark(landmark)
+                .build();
+        landmarkFeeRepository.save(landmarkFee);
     }
 
     @Test
@@ -90,7 +105,73 @@ class LandmarkFeeServiceTest {
         ResponseVo responseVo = landmarkFeeService.landmarkFeeInsert(landmarkFeeProcessRequest);
 
         //then
-        Assertions.assertEquals(SUCCESS_RESPONSE_VO, responseVo);
+        assertEquals(SUCCESS_RESPONSE_VO, responseVo);
+
+    }
+
+    @Test
+    void LANDMARK_FEE_DELETE_TEST() {
+        //given setup()
+
+
+        //when
+        ResponseVo responseVo = landmarkFeeService.landmarkFeeDelete(landmarkFee.getId());
+
+        //then
+        assertEquals(SUCCESS_RESPONSE_VO, responseVo);
+
+    }
+
+    @Test
+    void LANDMARK_FEE_ALERT_TEST() {
+        //given
+        LandmarkFeeModifyDto landmarkFeeModifyDto = LandmarkFeeModifyDto.builder()
+                .landmarkFeeId(landmarkFee.getId())
+                .title("changeLandmarkFee")
+                .price(1000)
+                .build();
+
+        //when
+        ResponseVo responseVo = landmarkFeeService.landmarkFeeAlert(landmarkFeeModifyDto);
+
+        //then
+        assertEquals(SUCCESS_RESPONSE_VO, responseVo);
+
+    }
+
+    @Test
+    void LANDMARK_FEE_LIST_SELECT_TEST() {
+        //given
+        List<LandmarkFee> landmarkFeeList = new ArrayList<>();
+        landmarkFeeList.add(landmarkFee);
+
+        LandmarkFeeListVo landmarkFeeListVo1 = new LandmarkFeeListVo(landmarkFeeList);
+
+        //when
+        LandmarkFeeListVo landmarkFeeListVo2 = landmarkFeeService.landmarkFeeListSelect(landmark.getId());
+
+        //then
+        assertThat(landmarkFeeListVo1)
+                .usingRecursiveComparison()
+                .isEqualTo(landmarkFeeListVo2);
+
+    }
+
+    @Test
+    void LANDMARK_FEE_ALL_LIST_SELECT_TEST() {
+        //given
+        List<LandmarkFee> landmarkFeeList = new ArrayList<>();
+        landmarkFeeList.add(landmarkFee);
+
+        LandmarkFeeAllListVo landmarkFeeAllListVo1 = new LandmarkFeeAllListVo(landmarkFeeList);
+
+        //when
+        LandmarkFeeAllListVo landmarkFeeAllListVo2 = landmarkFeeService.landmarkFeeAllListSelect();
+
+        //then
+        assertThat(landmarkFeeAllListVo1)
+                .usingRecursiveComparison()
+                .isEqualTo(landmarkFeeAllListVo2);
 
     }
 }
